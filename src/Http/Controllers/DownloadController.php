f30208e08e61993d05cefcd12732656e3182bd77
Сергей Инденбом <dzorogh@gmail.com>
2022-02-21 17:30:39 +0300

    Export format

--- src/Http/Controllers/DownloadController.php
+++ src/Http/Controllers/DownloadController.php
@@ -4,12 +4,11 @@ namespace Dzorogh\ExcelDataUpdate\Http\Controllers;
 
 use Dzorogh\ExcelDataUpdate\ExportHandler;
 use Maatwebsite\Excel\Facades\Excel;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class DownloadController
 {
-    public function __invoke(ExportHandler $exportHandler): BinaryFileResponse
+    public function __invoke(ExportHandler $exportHandler): \Symfony\Component\HttpFoundation\BinaryFileResponse
     {
-        return Excel::download($exportHandler->getExport(), 'users.xlsx');
+        return Excel::download($exportHandler->getExport(), 'export.xlsx', \Maatwebsite\Excel\Excel::XLSX);
     }
 }
